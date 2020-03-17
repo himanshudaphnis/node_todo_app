69157decb33db0c0e5b09fda765c70cf7c8faef7
himanshu khare <khareh57@gmail.com>
2020-03-17 19:32:14 +0530

    my first commit in node todo app

---
+++ controllers/todoController.js
@@ -0,0 +1,15 @@
+var data = [
+  { item: "get milk" },
+  { item: "press clothes" },
+  { item: "do some coding" }
+];
+
+module.exports = function(app) {
+  app.get("/todo", function(req, res) {
+    res.render("todo", { todos: data });
+  });
+
+  app.post("/todo", function(req, res) {});
+
+  app.delete("/todo", function(req, res) {});
+};
